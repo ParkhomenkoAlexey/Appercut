a99c7ed4820c1514ba5f4957d93fa3c1d9f7b1a7
Alexey Parkhomenko <alexey@tap.pm>
2022-11-03 14:52:34 +0300

    Refactor code

--- CustomAnimation/ViewController.swift
+++ CustomAnimation/ViewController.swift
@@ -18,48 +18,36 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .orange
+        view.backgroundColor = .white
         playVideo()
-        
-//        playerLayer.frame = CGRect(x: 180, y: 80, width: 240, height: 240)
-//        playerLayer.borderColor = UIColor.green.cgColor
-//        playerLayer.borderWidth = 2.0
-//        playerLayer.cornerRadius = 0
-//        playerLayer.backgroundColor = UIColor.systemPink.cgColor
-//        playerLayer.masksToBounds = true
-//        self.view.layer.addSublayer(playerLayer)
-        
-//        let animation = self.createPositionAnimation()
-//        self.playerLayer.add(animation, forKey: #keyPath(CALayer.position))
-//
-//        let tanimation = self.createTransformAnimation()
-//        self.playerLayer.add(tanimation, forKey: #keyPath(CALayer.transform))
-        
     }
     
-    private func createPositionAnimation() -> CABasicAnimation {
-        let initialValue = playerLayer.position
-        let finalValue: CGPoint = CGPoint(x: UIScreen.main.bounds.width - 200, y: UIScreen.main.bounds.height - 200)
-        CATransaction.setDisableActions(true)
-        playerLayer.position = finalValue
-//        playerLayer.sublayers?.first?.position = finalValue
-        let animation = CABasicAnimation(keyPath: #keyPath(CALayer.position))
-        animation.fromValue = initialValue
-        animation.toValue = finalValue
-        animation.duration = 4
-        return animation
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        self.removePeriodicTimeObserver()
     }
     
-    private func createTransformAnimation() -> CABasicAnimation {
-        let initialValue = playerLayer.transform
+    // MARK: - CALuaer Animations Setup
+    private func createTransformandPositionAnimations() -> (CABasicAnimation,CABasicAnimation) {
+        let transformInitialValue = playerLayer.transform
+        
+        let positionInitialValue = playerLayer.position
+        let positionFinalValue: CGPoint = CGPoint(x: UIScreen.main.bounds.width - 100, y: UIScreen.main.bounds.height - 100)
+        
         CATransaction.setDisableActions(true)
+        playerLayer.position = positionFinalValue
         playerLayer.transform = CATransform3DMakeScale(0.15, 0.15, 0.15)
-//        playerLayer.sublayers?.first?.transform = CATransform3DMakeScale(0.5, 0.5, 0.5)
-        let animation = CABasicAnimation(keyPath: #keyPath(CALayer.transform))
-        animation.fromValue = initialValue
-        animation.toValue = CATransform3DMakeScale(0.15, 0.15, 0.15)
-        animation.duration = 4
-        return animation
+        
+        let positionAnimation = CABasicAnimation(keyPath: #keyPath(CALayer.position))
+        positionAnimation.fromValue = positionInitialValue
+        positionAnimation.toValue = positionFinalValue
+        positionAnimation.duration = 4
+        
+        let transformAnimation = CABasicAnimation(keyPath: #keyPath(CALayer.transform))
+        transformAnimation.fromValue = transformInitialValue
+        transformAnimation.toValue = CATransform3DMakeScale(0.15, 0.15, 0.15)
+        transformAnimation.duration = 4
+        return (transformAnimation, positionAnimation)
     }
     
     private func createCornerAnimation() -> CABasicAnimation {
@@ -68,13 +56,14 @@ class ViewController: UIViewController {
         CATransaction.setDisableActions(true)
         playerLayer.cornerRadius = finalValue
         playerLayer.sublayers?.first?.cornerRadius = finalValue
-        let animation = CABasicAnimation(keyPath: "cornerRadius")
+        let animation = CABasicAnimation(keyPath: #keyPath(CALayer.cornerRadius))
         animation.fromValue = initialValue
         animation.toValue = finalValue
         animation.duration = 0.5
         return animation
     }
     
+    // MARK: - Timer Setup
     private func addPeriodicTimeObserver() {
         
         let timeScale = CMTimeScale(NSEC_PER_SEC)
@@ -88,32 +77,27 @@ class ViewController: UIViewController {
             switch intSeconds {
             case 3:
                 let cornerAnimation = self.createCornerAnimation()
-                self.playerLayer.add(cornerAnimation, forKey: "cornerRadius")
-                self.playerLayer.sublayers?.first?.add(cornerAnimation, forKey: "cornerRadius")
+                self.playerLayer.add(cornerAnimation, forKey: #keyPath(CALayer.cornerRadius))
+                self.playerLayer.sublayers?.first?.add(cornerAnimation, forKey: #keyPath(CALayer.cornerRadius))
             case 4:
-//                let transformAnimation = self.createTransformAnimation()
-                let positionAnimation = self.createPositionAnimation()
+                let animations = self.createTransformandPositionAnimations()
                 
-                CATransaction.begin()
-//                self.playerLayer.add(transformAnimation, forKey: #keyPath(CALayer.position))
-//                self.playerLayer.sublayers?.first?.add(transformAnimation, forKey: #keyPath(CALayer.transform))
-                self.playerLayer.add(positionAnimation, forKey: #keyPath(CALayer.position))
-                CATransaction.commit()
-                
-                
-                
-//                self.playerLayer.sublayers?.first?.add(positionAnimation, forKey: #keyPath(CALayer.position))
-                
-                
-                print ("VIDEO PLAYED FOR 4 SECONDS")
-            case 5:
-                print ("VIDEO PLAYED FOR 5 SECONDS")
+                self.playerLayer.add(animations.0, forKey: #keyPath(CALayer.transform))
+                self.playerLayer.add(animations.1, forKey: #keyPath(CALayer.position))
             default:
                 break
             }
         }
     }
     
+    private func removePeriodicTimeObserver() {
+        if let timeObserverToken = timeObserverToken {
+            player.removeTimeObserver(timeObserverToken)
+            self.timeObserverToken = nil
+        }
+    }
+    
+    // MARK: - AVPlayer Setup
     private func playVideo() {
         guard let path = Bundle.main.path(forResource: "test_1", ofType:"mp4") else {
             debugPrint("test_1.mp4 not found")
@@ -121,7 +105,6 @@ class ViewController: UIViewController {
         }
         
         self.player = AVPlayer(url: URL(fileURLWithPath: path))
-        // UIScreen.main.bounds.height
         self.playerLayer = AVPlayerLayer(player: player)
         self.view.layer.addSublayer(playerLayer)
         self.playerLayer.frame = CGRect(x: 0,
@@ -129,7 +112,7 @@ class ViewController: UIViewController {
                                         width: UIScreen.main.bounds.height,
                                         height: UIScreen.main.bounds.height)
         self.playerLayer.position = view.center
-        self.playerLayer.backgroundColor = UIColor.red.cgColor
+        self.playerLayer.backgroundColor = UIColor.white.cgColor
         
         self.playerLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
 
